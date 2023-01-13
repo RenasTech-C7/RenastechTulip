dd68f27842fc53bc94a4278beb3012c9bd7e8ed1
oralridvan <oral.ridvan@hotmail.com>
2023-01-12 20:27:50 -0500

    Cucumber Day 03 Part 1

--- 04_CucumberBDD/src/test/java/code/stepsDefinitions/guru99Steps.java
+++ 04_CucumberBDD/src/test/java/code/stepsDefinitions/guru99Steps.java
@@ -42,6 +42,33 @@ public class guru99Steps extends BrowserUtils {
         guru99OrderSuccess.setVerificationText("Payment successfull!");
     }
 
+    @When("The user wants to buy elephant toy as {string}")
+    public void the_user_wants_to_buy_elephant_toy_as(String string) {
+        guru99Home.getQuantityDropdown(string);
+        guru99Home.getBuyNowButton();
+    }
+    @Then("The user wants to enter Credit Card Number as {string}")
+    public void the_user_wants_to_enter_credit_card_number_as(String string) {
+        guru99Payment.setCardNumber(string);
+    }
+    @Then("The user wants to enter Expiration Month as {string}")
+    public void the_user_wants_to_enter_expiration_month_as(String string) {
+        guru99Payment.setMonth(string);
+    }
+    @Then("The user wants to enter Expiration Year as {string}")
+    public void the_user_wants_to_enter_expiration_year_as(String string) {
+        guru99Payment.setYear(string);
+    }
+    @Then("The user wants to enter CVV Code as {string}")
+    public void the_user_wants_to_enter_cvv_code_as(String string) {
+        guru99Payment.setCvv_code(string);
+    }
+    @Then("The user wants to verify message as {string}")
+    public void the_user_wants_to_verify_message_as(String string) {
+        guru99OrderSuccess.setVerificationText(string);
+    }
+
+
 
 
 }
